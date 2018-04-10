2192a6a1802a47c7ebbfb93ad546caffa29558c9
Igor Neves Faustino <igornfaustino@gmail.com>
2018-04-09 23:21:19 -0300

    add react router
    setup react router

---
+++ src/pages/Main.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+{/* EXAMPLE... To see more...
+ https://medium.com/@pshrmn/a-simple-react-router-v4-tutorial-7f23ff27adf
+*/}
+
+
+// IMPORT HERE THE COMPONENTS THAT WILL BE RENDER!
+// import Home from './Home'
+// import Roster from './Roster'
+// import Schedule from './Schedule'
+
+/**
+ * Main page, here you can pass your path and the componet to render...
+ */
+const Main = () => (
+	<main>
+		<Switch>
+			{/* <Route exact path='/' component={Home} />
+			<Route path='/roster' component={Roster} />
+			<Route path='/schedule' component={Schedule} /> */}
+		</Switch>
+	</main>
+);
+
+export default Main;
