834e8c2bb07fe867f355c9bbf73089cdf03b41de
Letícia Mazzo Portela <leticiaportela@alunos.utfpr.edu.br>
2018-04-18 18:29:11 -0300

    Add products page

---
+++ src/pages/AdminEditRequests.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class AdminEditRequests extends React.Component {
+	render() {
+		return (
+			<div>
+				<h2>Editar Solicitações</h2>
+			</div >
+		);
+	}
+}
