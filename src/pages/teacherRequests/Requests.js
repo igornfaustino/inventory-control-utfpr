45465be57b1d59f112783b0966ea15d478954539
gabriel_choptian@hotmail.com <gabriel_choptian@hotmail.com>
2018-05-06 01:44:40 -0300

    Revert "Tela de Editar Solicitação completa"
    This reverts commit d6113dccf833184e4d0e29a0344637c8ea354cce.

--- src/pages/teacherRequests/Requests.js
+++ src/pages/teacherRequests/Requests.js
@@ -7,7 +7,6 @@ import '../Pages.css';
 import TableList from '../../components/TableList/TableList';
 import SubHeader from '../../components/SubHeader/SubHeader';
 
-import {Link} from 'react-router-dom';
 import axios from 'axios';
 import moment from 'moment'
 
@@ -40,9 +39,9 @@ export default class ApprovedRequests extends React.Component {
 						// siorg: item.siorg,
 						description: item.description,
 						date: moment(item.date).locale('pt-br').format('DD/MM/YYYY'),
-						input: (<Link to={`editarsolicitacoes/${item._id}`}>
-						Editar
-					  </Link>)
+						input: (<Button color="success" onClick={() => {
+							this.handleClick(item)
+						}} type="submit">Solicitar</Button>)
 					})
 				})
 				// items = items.filter(item => {
